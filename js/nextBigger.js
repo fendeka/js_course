b7d56f3c561fafb036fa5c0122baf46f371ce0f1
fenda <fendeka@mail.ru>
2017-03-20 21:55:33 +0200

    object with functions

--- js/nextBigger.js
+++ js/nextBigger.js
@@ -9,9 +9,52 @@ var nextBigger = function (number) {
             return digits.join('');
         }
     }
-    return digits;
+    return digits.join('');
 };
 
-var out = nextBigger(513);
+var out = nextBigger(53);
 
 console.log(out);
+
+var MyObject = {
+
+    property: function (name, value) {
+        Object.defineProperty(this, name,  {
+            enumerable: true,
+            configurable: true,
+            writable: true,
+            value: value
+        })
+    },
+
+    constant: function (name, value) {
+        Object.defineProperty(this, name,  {
+            enumerable: true,
+            configurable: false,
+            writable: false,
+            value: value
+        })
+    },
+
+    hidden: function (name, value) {
+        Object.defineProperty(this, name,  {
+            enumerable: false,
+            configurable: true,
+            writable: true,
+            value: value
+        })
+    },
+
+    values: function () {
+        return Object.values(this);
+    }
+
+};
+
+MyObject.property('test1', 123);
+MyObject.hidden('test2', 123);
+MyObject.constant('test3', 123);
+
+console.log(MyObject);
+console.log(MyObject.values());
+
